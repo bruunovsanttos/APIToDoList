0aa9bec353132ddd040e72155e6f2c50df143d63
bruunovsanttos <bruno-vieira-santos@hotmail.com>
2025-02-06 12:32:54 -0300

    feat:adicionando estancia e funções de banco de dados no modelo de tarefa

--- Models/task.py
+++ Models/task.py
@@ -1,14 +1,14 @@
+from sqlalchemy import banco
 #aqui eu crio o modelo de task
 #serão necerrios ID, titulo e descrição
 
 class Task(): #importar banco de dados
     #colocar como deve ser estruturado o banco de dados
+    __tablename__ = "tarefas"
 
-
-
-
-
-
+    id_task = banco.Collumn(banco.Integer, primary_key=True)
+    title = banco.Collumn(banco.String(50))
+    description = banco.Collumn(banco.String(500))
 
     #______________________________________________________
 
@@ -24,3 +24,18 @@ class Task(): #importar banco de dados
             'title': self.title,
             'description': self.description
         }
+
+    def save_task(self):
+        banco.session.add(self)
+        banco.session.commit()
+
+    def delete_task(self):
+        banco.session.delete()
+        banco.session.commit()
+
+    @classmethod
+    def find_task(cls, id_task):
+        task = cls.query.filter_by(id_task=id_task).first()
+        if task:
+            return task
+        return None 
