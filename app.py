dcfe26e334477742bf9ac755f375b112d416d0ca
bruunovsanttos <bruno-vieira-santos@hotmail.com>
2025-02-11 12:44:50 -0300

    feat: adicionando configurações da api no app.py

--- app.py
+++ app.py
@@ -3,4 +3,23 @@ from flask_restful import Api
 # importar os resources
 import os
 
+base_dir = os.path.dirname(os.path.abspath(__file__))
+database_path = os.path.join(base_dir, "banco.db")
 
+app = Flask(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{database_path}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+api = Api(app)
+
+api.add_resource(Task, '/tasks')
+
+
+if __name__ == '__main__':
+    from sqlalchemy import banco
+    banco.init_app(app)
+    with app.app_context():
+        banco.create_all()
+
+
+    app.run(debug=True)
+    
