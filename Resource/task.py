2052e36f60808f1fdc7f0207e348c4f04f25398e
bruunovsanttos <bruno-vieira-santos@hotmail.com>
2025-02-14 12:56:10 -0300

    feat: adicionando caminho para mostrar tasks sem id

--- Resource/task.py
+++ Resource/task.py
@@ -9,11 +9,20 @@ class TaskResource(Resource):
     arguments.add_argument('title', type=str, required= True, help= "O campo title não pode ser deixado em branco.")
     arguments.add_argument('description', type=str, required= True, help= "O campo description não pode ser deixado em branco")
 
-    def get(self, id_task):
-        task = Task.find_task(id_task)
-        if task:
-            return task.json(), 200
-        return {'message': 'task not found'}, 404
+    def get(self, id_task=None):
+        if id_task:
+            task = Task.find_task(id_task)
+            if task:
+                return task.json(), 200
+            return {'message': 'task not found'}, 404
+        else:
+            tasks = Task.query.all() #chama todas as tasks do banco de dados
+            task_list = [] #lista para adicionar cada task encontrada.
+
+            for task in tasks: #se tem uma task em task
+                task_list.append(task.json()) #adiciona no task_list
+            return task_list, 200 #mostra a lista e retorna a requisição
+
 
     def post(self):
 
