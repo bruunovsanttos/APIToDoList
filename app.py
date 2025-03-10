b8a9dc9b2dfb8e98dbcda23448fcfa855a766e13
bruunovsanttos <bruno-vieira-santos@hotmail.com>
2025-03-10 12:39:21 -0300

    feat: iniciando criação de token com jwt

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from Resource.task import TaskResource
 from Resource.user import UsuarioResource
 from extensions import banco
 import os
+from flask_jwt_extended import JWTManager
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 database_path = os.path.join(base_dir, "banco.db")
@@ -11,7 +12,10 @@ database_path = os.path.join(base_dir, "banco.db")
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{database_path}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'DontTellAnyone'
 api = Api(app)
+jwt = JWTManager(app)
+
 
 api.add_resource(TaskResource, '/tasks', '/tasks/<int:id_task>') #dessa forma ele aceita o endpoint para tasks com ou sem id
 api.add_resource(UsuarioResource, '/user', '/user/<int:id_user>')
