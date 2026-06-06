13b2b54b23e82c8d9fca12810039a1746772b27b
bruunosanttos <bruno-vieira-santos@hotmail.com>
2026-06-06 02:25:58 -0300

    criando ação do botão para sair do dashboard

--- app/static/js/tasks.js
+++ app/static/js/tasks.js
@@ -1,1 +1,23 @@
 
+const token = localStorage.getItem("token");
+const taskForm = document.getElementById("task-form");
+const tasksList = document.getElementById("tasks-list");
+const logoutButton = document.getElementById("logout");
+
+if (!token) {
+    window.location.href = "/";
+}
+
+if (logoutButton) {
+    logoutButton.addEventListener("click", async function () {
+        await fetch("/logout", {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        });
+
+        localStorage.removeItem("token");
+        window.location.href = "/";
+    });
+}
