2ba47cae06bee1d83e20bf926fe4377a7e0c1e5a
bruunosanttos <bruno-vieira-santos@hotmail.com>
2026-06-05 23:33:46 -0300

    adicionadas rotas de paginas ja criadas para a interface grafica

--- app/routes/web.py
+++ app/routes/web.py
@@ -2,3 +2,14 @@ from flask import Blueprint, render_template
 
 web_bp = Blueprint("web", __name__)
 
+@web_bp.route("/")
+def login_page():
+    return render_template("login.html")
+
+@web_bp.route("/register")
+def register_page():
+    return render_template("register.html")
+
+@web_bp.route("/dashboard")
+def dashboard_page():
+    return render_template("/dashboard.html")
